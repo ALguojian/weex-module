bddb65c4f1c2ac5dd1ba37499a9b87ff8d66cbc5
ALguojian <3324883037@qq.com>
2018-06-20 11:46:36 +0800

    ..

--- app/src/main/java/com/alguojian/example/MainActivity.java
+++ app/src/main/java/com/alguojian/example/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.shuwei.weex.ui.WeexPagerActivity;
+import com.shuwei.weex.ui.WeexPagerFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +14,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         WeexPagerActivity.start(this,"home/home.js");
+
+        WeexPagerFragment.newInstance("home/home.js");
     }
 }
